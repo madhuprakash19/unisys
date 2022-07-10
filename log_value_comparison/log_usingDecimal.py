8d0b2a239012a67f23c56d29bb88b62579f6b202
LAKSHANA2001 <lakshanatangatur@gmail.com>
2022-07-10 15:51:16 -0400

    changed

--- log_value_comparison/log_usingDecimal.py
+++ log_value_comparison/log_usingDecimal.py
@@ -2,7 +2,7 @@ import math
 import datetime
 from decimal import Decimal
 start=datetime.datetime.now()
-for i in range(450,460+1):
+for i in range(1,10):
     print(Decimal(i).ln() /Decimal(10).ln())
 end=datetime.datetime.now()
 print("Total time taken is: ",end-start," milli seconds")
