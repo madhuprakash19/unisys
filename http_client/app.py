5782958dfa19dc0863d69a6f5334f404a95f49d1
LAKSHANA2001 <lakshanatangatur@gmail.com>
2022-09-25 22:54:19 +0530

    log

--- http_client/app.py
+++ http_client/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, render_template, request
-
 app = Flask(__name__)
 course = [
     {"id": 1, "name": "Lakshana"},
--- log_value_comparison/log_usingMath.py
+++
@@ -1,8 +0,0 @@
-import math
-import datetime
-from decimal import Decimal
-start=datetime.datetime.now()
-for i in range(1,100):
-    print(i ," - ",math.log(i,10))
-end=datetime.datetime.now()
-print("Total time taken is: ",end-start," milli seconds")
